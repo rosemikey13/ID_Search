55a5739e568cd4eb398a4838047e083e3ce2fd16
Michael Rose <rosemikey13@aol.com>
2022-05-27 16:54:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Page from "./components/Page"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const records =[71, 82, 100, 93]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Page records={records}/>
   );
 }
 
